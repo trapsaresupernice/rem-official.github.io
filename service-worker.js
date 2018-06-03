0876f433fa1f20c85dcc3082889990f930704dad
lee.wp14 <lee.wp14@gmail.com>
2018-06-03 13:18:29 +0800

    test commit

--- service-worker.js
+++ service-worker.js
@@ -37,6 +37,7 @@ self.addEventListener("activate", function (event) {
 });
 
 navigator.serviceWorker.getRegistrations().then(
+
     function(registrations) {
         for(let registration of registrations) {  
             registration.unregister();
